7d2b000281366951fcb26a2082bbbc10ac14ef88
izalfront <rizalhajakubastarus@gmail.com>
2024-01-11 20:46:34 +0800

    login page done

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -5,10 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
+    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
+    <link rel="stylesheet" href="{{ asset('style/style.css') }}">
+
 </head>
 
 <body>
-    hello worrld
+    <div class="login-container">
+        <div class="img-column">
+            <img src="{{ asset('/Logo.jpg') }}" style="max-width: 100%;">
+        </div>
+        <div class="form-column">
+            <form action="submit.php" method="post">
+                <h1 class="judul">Simagang BRI</h1>
+                <div class="form-group">
+                    <label for="username" class="input__form">Username:</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="input__form">Password:</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
